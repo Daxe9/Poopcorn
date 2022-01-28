e8913422df572162bd394fc4b1f79b0344aa46b9
Daxe9 <davidexie694@gmail.com>
2022-01-28 23:28:31 +0100

    Add a mongoDB schema for poop

--- backend/main.js
+++ backend/main.js
@@ -2,7 +2,18 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-
+const mongoose = require("mongoose");
+const db = process.env.DB;
+mongoose
+    .connect(db, {
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log(`Connected to ${db}.`);
+    })
+    .catch((e) => {
+        console.error("Error ", e);
+    });
 app.get("/", (req, res) => {
     res.status(200).send("Ciao Mondo");
 });
