508b6020b96e338963277facd422b3f948e03864
Daxe9 <davidexie694@gmail.com>
2022-02-06 12:03:24 +0100

    updated cors policy to allow connection

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,11 @@ const request = require("./middleware/request");
 const poops = require("./routes/poops");
 const users = require("./routes/users");
 const login = require("./routes/login");
+const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const db = process.env.DB;
-
+app.use(cors())
 app.use(express.json());
 app.use(request);
 app.use("/api/poops", poops);
